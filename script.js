8fc6ac517f49b0fa446cb8e2caf5e6a0248f85dc
isab3498 <isabella.boisen.95@gmail.com>
2019-06-18 10:53:54 +0200

    hover, faver menu ikon andre små ændringer - sitet mangler kun indhold

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ function sidenVises() {
     console.log("sidenVises");
 
     document.querySelector("#menuknap").addEventListener("click", toggleMenu);
-
-
 }
 
 
@@ -17,8 +15,10 @@ function toggleMenu() {
 
     if (erSkjult == true) {
         document.querySelector("#menuknap").textContent = "☰";
+        document.querySelector("#menuknap").style.color = "white";
     } else {
         document.querySelector("#menuknap").textContent = "X";
+        document.querySelector("#menuknap").style.color = "#c6c9c9";
 
     }
 }
